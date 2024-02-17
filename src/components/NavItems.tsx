bfda129a9cda30787f044805f09bb1158f4fccfb
Damilola Adebowale <adebowaletemitayo40@gmail.com>
2024-02-17 19:25:01 +0100

    Finsished implementing the Navbar and the side cart

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,15 +1,34 @@
 "use client";
 import { PRODUCT_CATEGORIES } from "@/config";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import NavItem from "./NavItem";
+import { useOnClickOutside } from "@/hooks/use-on-click-outside";
 
 const NavItems = () => {
   const [activeIndex, setActiveIndex] = useState<null | number>(null);
 
+  // This wraps the clicking outside evenst in the webpage both for clicking and using of esc keypress
+  const navRef = useRef<HTMLDivElement>(null);
+  useOnClickOutside(navRef, () => setActiveIndex(null));
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        setActiveIndex(null);
+      }
+    };
+
+    document.addEventListener("keydown", handler);
+
+    return () => {
+      document.removeEventListener("keydown", handler);
+    };
+  }, []);
+
+  // That is, its already open
   const isAnyOpen = activeIndex !== null;
 
   return (
-    <div className="flex gap-4 h-full">
+    <div className="flex gap-4 h-full" ref={navRef}>
       {PRODUCT_CATEGORIES.map((category, i) => {
         const handleOpen = () => {
           if (activeIndex === i) {
